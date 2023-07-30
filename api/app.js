5bb19d00e0436843d6cc07bd8fab9f06b5b84724
Samer99Najjar <80600556+Samer99Najjar@users.noreply.github.com>
2023-07-30 10:42:09 +0300

    Initial commit for frontend

--- api/app.js
+++ api/app.js
@@ -10,6 +10,13 @@ const {List,Task} = require('./db/models');
 
 app.use(bodyparser.json());
 
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 app.get('/lists', async (req, res) => {
     try {
       const lists = await List.find({}).lean();
